57823ea4af1bd84be64c1e524ff04d8454294acf
wndgur2 <wndgur76@naver.com>
2025-07-17 10:55:25 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+})
